9182032834cab0575d1fda6465cee6801d5d160d
Alexander Pronin <pronin.alx@gmail.com>
2016-09-27 16:11:43 +0300

    Final commit

--- beginningswift_variablechallenge.playground/Contents.swift
+++ beginningswift_variablechallenge.playground/Contents.swift
@@ -8,13 +8,23 @@
 import UIKit
 
 //: Declare a constant of type Int called myAge and set it to your age.
-
+let myAge: Int = 40
 
 //: Declare a variable of type Double called averageAge. Initially, set it to your own age. Then, set it to the average of your age and my own age of 30.
+var averageAge: Double = 40
+averageAge = 35
 
 
 //: Create a variable called answer and initialize it with the value 0. Increment it by 1. Add 10 to it. Multiply it by 10 and divide by 3. After all of these operations, what’s the answer?
+var answer = 0
+answer += 1
+answer += 10
+answer *= 10
+answer /= 3
 
 //: Declare two constants a and b of type Double and assign both a value. Calculate the average of a and b and store the result in a constant named average.
+let a: Double = 123.1
+let b: Double = 765.22
+let average = (a + b)/2
 
 
